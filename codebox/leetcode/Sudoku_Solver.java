9767be160389b455fcbc94c12a149c0784d610e8
wjlei <wjlei@wjleitekiMacBook-Air.local>
2013-09-11 22:00:29 +0800

    change

--- codebox/leetcode/Sudoku_Solver.java
+++ codebox/leetcode/Sudoku_Solver.java
@@ -28,6 +28,26 @@ public class Sudoku_Solver {
         }
         return res;       
     }
+    public static boolean solve(char[][] board){
+		int n=board.length;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n;j++){
+				if(board[i][j]=='.'){
+					for(char numchar='1';numchar<='9';numchar++){
+						board[i][j]=numchar;
+						if(check(board,i,j)){
+							if(solve(board))
+								return true;
+						}
+					}
+					board[i][j]='.';
+					return false;
+				}
+			}
+		}
+		return true;
+		
+	}
     
     public static boolean solve(char[][] board){
 	boolean isfind=false;
