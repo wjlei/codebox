20b8dc8dc3c446ec7aa7ddc03d2a778f39886c63
wjlei <wjleibiz@gmail.com>
2014-01-09 17:05:43 +0800

    change

--- codebox/python/log_analysis.py
+++ codebox/python/log_analysis.py
@@ -25,22 +25,29 @@ def analisysfile(file):
     try :
         for line in input:
             if line.find('serialNumber')!=-1:
-                sn=re.search('serialNumber *: *(\d+)',line).group(1);
+                if re.search('serialNumber *:* *(\d+)',line)==None:
+                    continue;
+                sn=re.search('serialNumber *:* *(\d+)',line).group(1);
                 #ts=datetime.datetime.strptime('2013-11-06 15:00:18.429','%Y-%m-%d %H:%M:%S.%f')
                 #print(ts.strftime('%Y-%m-%d %H:%M:%S.%f'))
                 ts=datetime.datetime.strptime(re.search('(\d+-*)+ (\d+:*)+.\d+',line).group(0),'%Y-%m-%d %H:%M:%S.%f');
-                if line.find('Recieve')!=-1:
+                if line.find('Received an event')!=-1:
                     a=Aline();
                     a.sn=int(sn);a.rts=ts;a.rline=line;
                     dic[int(sn)]=a;
-                elif line.find('EmitEvent')!=-1:
-                    a=dic[int(sn)];
-                    a.sts=ts;a.sline=line;
+                elif line.find('EventEmitter')!=-1:
+                    if dic.has_key(int(sn)):
+                        a=dic[int(sn)];
+                        a.sts=ts;a.sline=line;
+                    else:
+                        a=Aline();
+                        a.sn=int(sn);a.sts=ts;a.sline=line;
+                        dic[int(sn)]=a;
         for i in range(0,len(dic)-1):
             if(dic[i].rline!=None):
                 output.write(dic[i].rline);
             else:
-                output.write('missing');
+                output.write('missing\n');
             if(dic[i].sline!=None):
                 output.write(dic[i].sline);
             else:
@@ -53,17 +60,20 @@ def analisysfile(file):
                 count+=1;
         if(count!=0):
             print(sum/count)
+        else:
+            print('None');
     finally:
         input.close();
         output.close();
         print('processing');
 
-logdir='/Users/wjlei/testdata/log';
+logdir='D:\\INAS\\log';
 filelist=os.listdir(logdir);
-#today=time.strftime('%Y%m%d',time.localtime(time.time()));
-today='20131106';
+today=time.strftime('%Y%m%d',time.localtime(time.time()));
+#today='20131106';
 for filename in filelist:
     if filename.find(today)!=-1 and filename.find('result')==-1:
         analisysfile(logdir+'/'+filename);
 print('done');
     
+
